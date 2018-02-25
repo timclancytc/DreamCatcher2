c3a1c041e0464cf38f349e67a3eaab451ce0e889
timclancytc <timclancytc@users.noreply.github.com>
2018-02-24 21:55:38 -0500

    Initial commit

---
+++ app/src/main/java/cs5254/cs/vt/edu/dreamcatcher2/DreamActivity.java
@@ -0,0 +1,13 @@
+package cs5254.cs.vt.edu.dreamcatcher2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DreamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dream);
+    }
+}
